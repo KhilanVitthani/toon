dcb65a3fa85f9fc7e3538c705a8c769e28119b9d
Khilan <khilanvithani0080@gmail.com>
2023-01-03 17:24:37 +0530

    All Done.

--- lib/app/modules/my_collection_page/views/my_collection_page_view.dart
+++ lib/app/modules/my_collection_page/views/my_collection_page_view.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:yodo1mas/Yodo1MasBannerAd.dart';
@@ -37,88 +37,101 @@ class MyCollectionPageView extends GetWidget<MyCollectionPageController> {
         }
         return await true;
       },
-      child: Scaffold(
-        backgroundColor: appTheme.primaryTheme,
-        appBar: AppBar(
+      child: SafeArea(
+        child: Scaffold(
           backgroundColor: appTheme.primaryTheme,
-          title: Text(
-            'My Collection',
-            style: GoogleFonts.karla(
-              fontSize: MySize.getHeight(24),
-              fontWeight: FontWeight.w500,
+          appBar: AppBar(
+            backgroundColor: appTheme.primaryTheme,
+            title: Text(
+              'My Collection',
+              style: GoogleFonts.karla(
+                fontSize: MySize.getHeight(24),
+                fontWeight: FontWeight.w500,
+              ),
             ),
-          ),
-          centerTitle: true,
-          leading: GestureDetector(
-            onTap: () async {
-              if (getIt<TimerService>().is40SecCompleted) {
-                await getIt<AdService>()
-                    .getAd(adType: AdService.interstitialAd)
-                    .then((value) {
-                  if (!value) {
-                    getIt<TimerService>().verifyTimer();
-                    SystemChrome.setEnabledSystemUIMode(
-                        SystemUiMode.edgeToEdge);
-                    Get.offAndToNamed(Routes.MAIN_SCREEN);
-                  }
-                });
-              } else {
-                Get.offAndToNamed(Routes.MAIN_SCREEN);
-              }
-            },
-            child: Container(
-              padding: EdgeInsets.only(left: MySize.getWidth(10)),
-              child: Icon(Icons.arrow_back),
+            centerTitle: true,
+            leading: GestureDetector(
+              onTap: () async {
+                if (getIt<TimerService>().is40SecCompleted) {
+                  await getIt<AdService>()
+                      .getAd(adType: AdService.interstitialAd)
+                      .then((value) {
+                    if (!value) {
+                      getIt<TimerService>().verifyTimer();
+                      SystemChrome.setEnabledSystemUIMode(
+                          SystemUiMode.edgeToEdge);
+                      Get.offAndToNamed(Routes.MAIN_SCREEN);
+                    }
+                  });
+                } else {
+                  Get.offAndToNamed(Routes.MAIN_SCREEN);
+                }
+              },
+              child: Container(
+                padding: EdgeInsets.only(left: MySize.getWidth(10)),
+                child: Icon(Icons.arrow_back),
+              ),
             ),
+            elevation: 0,
           ),
-          elevation: 0,
-        ),
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: MySize.getWidth(10)),
-          child: Column(
-            children: [
-              Expanded(
-                child: Container(
-                  child: GridView.builder(
-                    padding: EdgeInsets.only(bottom: 10),
-                    itemCount: controller.myImage.length,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        crossAxisSpacing: MySize.getHeight(10.0),
-                        mainAxisSpacing: MySize.getHeight(10.0)),
-                    itemBuilder: (context, index) {
-                      return InkWell(
-                        onTap: () {
-                          Get.offAndToNamed(Routes.SHARE_FILE, arguments: {
-                            ArgumentConstant.capuredImage:
-                                File(controller.myImage[index]),
-                            ArgumentConstant.isFromMyCollection: true,
-                            ArgumentConstant.isFromHome: false,
-                          });
-                        },
+          body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: MySize.getWidth(10)),
+            child: Column(
+              children: [
+                (controller.myImage.isEmpty)
+                    ? Expanded(
+                        child: Center(
+                          child: SvgPicture.asset(imagePath + "404.svg",
+                              height: MySize.getHeight(109),
+                              width: MySize.getWidth(215)),
+                        ),
+                      )
+                    : Expanded(
                         child: Container(
-                          child: ClipRRect(
-                            borderRadius:
-                                BorderRadius.circular(MySize.getHeight(8)),
-                            child: Image.file(
-                              File(
-                                controller.myImage[index],
-                              ),
-                              fit: BoxFit.cover,
-                            ),
+                          child: GridView.builder(
+                            padding: EdgeInsets.only(bottom: 10),
+                            itemCount: controller.myImage.length,
+                            gridDelegate:
+                                SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 2,
+                                    crossAxisSpacing: MySize.getHeight(10.0),
+                                    mainAxisSpacing: MySize.getHeight(10.0)),
+                            itemBuilder: (context, index) {
+                              return InkWell(
+                                onTap: () {
+                                  Get.offAndToNamed(Routes.SHARE_FILE,
+                                      arguments: {
+                                        ArgumentConstant.capuredImage:
+                                            File(controller.myImage[index]),
+                                        ArgumentConstant.isFromMyCollection:
+                                            true,
+                                        ArgumentConstant.isFromHome: false,
+                                      });
+                                },
+                                child: Container(
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(
+                                        MySize.getHeight(8)),
+                                    child: Image.file(
+                                      File(
+                                        controller.myImage[index],
+                                      ),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
                           ),
                         ),
-                      );
-                    },
-                  ),
-                ),
-              ),
-              (controller.connectivityResult == ConnectionState.none)
-                  ? SizedBox()
-                  : Yodo1MASBannerAd(
-                      size: BannerSize.Banner,
-                    ),
-            ],
+                      ),
+                (controller.connectivityResult == ConnectionState.none)
+                    ? SizedBox()
+                    : Yodo1MASBannerAd(
+                        size: BannerSize.Banner,
+                      ),
+              ],
+            ),
           ),
         ),
       ),
